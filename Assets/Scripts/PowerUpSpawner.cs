a643d8d727e1006b1e6f0264a375e9b689b2d78a
FeixeDeLuz <luiz.mendescastro@gmail.com>
2025-08-31 21:34:12 -0300

    Reworking Enemy Spawn System (Working)

--- Assets/Scripts/PowerUpSpawner.cs
+++ Assets/Scripts/PowerUpSpawner.cs
@@ -6,22 +6,38 @@ public class PowerUpSpawner : MonoBehaviour
 {
 
      [SerializeField] private GameObject[] powerUp;
+     [SerializeField] private GameObject[] enemy;
+     [SerializeField] private Vector2[] enemySpawnPoints;
 
      void Start()
      {
           StartCoroutine(SpawnPowerUp());
+          StartCoroutine(SpawnEnemy());
      }
 
      IEnumerator SpawnPowerUp()
      {
           while (true)
           {
-               yield return new WaitForSeconds(Random.Range(5, 15));               
+               yield return new WaitForSeconds(Random.Range(5, 15));
                float x = Random.Range(-18f, 10f);
                float y = Random.Range(-18f, 10f);
-               Instantiate(powerUp[0], new Vector2(x, y), Quaternion.identity);               
+               Instantiate(powerUp[0], new Vector2(x, y), Quaternion.identity);
           }
      }
+
+     IEnumerator SpawnEnemy()
+     {
+          while (true)
+          {
+               yield return new WaitForSeconds(Random.Range(1, 4));
+               int index = Random.Range(0, enemySpawnPoints.Length);
+               Vector2 spawnPos = enemySpawnPoints[index];
+               Debug.Log("Spawning enemy at: " + spawnPos); 
+               Instantiate(enemy[0], spawnPos, Quaternion.identity);
+          }
+     }
+
 
 
 }
